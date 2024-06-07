435016a5aefb3ae37d0370b8c6aa7ac0fa2144f8
Orozali <ilyazovorozali08@gmail.com>
2024-06-07 22:29:19 +0600

    First Commit

---
+++ loans/src/main/java/com/easyBytes/loans/LoansApplication.java
@@ -0,0 +1,29 @@
+package com.easyBytes.loans;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EasyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Orozali Iliazov",
+						email = "ilyazovorozali08@gmail.com"
+				)
+		)
+)
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
